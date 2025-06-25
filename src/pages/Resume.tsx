1bc30242d0ce2b8b92c79bef252d60ab0c6ea715
Diego <ing.juandiego.vargas@gmail.com>
2025-06-24 22:46:25 -0700

    Height change

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -57,7 +57,7 @@ export const Resume = () => {
           {language === "en" ? "Education" : "Educación"}
         </button>
       </div>
-      <div className="h-[calc(100vh-150px)] overflow-auto flex-1 bg-neutral-100 p-2 rounded-md shadow-lg">
+      <div className="h-[calc(100vh-200px)] overflow-auto flex-1 bg-neutral-100 p-2 rounded-md shadow-lg">
         {activeSection === "AboutMe" ? (
           <AboutMe />
         ) : activeSection === "Experience" ? (
